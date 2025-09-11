0bff1515622829b703a2f9cfd9bb9e8c37987b57
Sergio Perez <se.perezr12@uniandes.edu.co>
2025-09-10 21:13:22 -0500

    Ajuste de carpetas

--- Ventas/src/modulos/producto/aplicacion/consultas/obtener_producto_por_id.py
+++
@@ -1,8 +0,0 @@
-
-from dataclasses import dataclass
-from seedwork.aplicacion.consultas import Consulta
-import uuid
-
-@dataclass
-class ObtenerProductoPorIdConsulta(Consulta):
-    id: uuid.UUID
--- Ventas/src/modulos/producto/dominio/repositorios.py
+++
@@ -1,8 +0,0 @@
-from seedwork.dominio.repositorios import Repositorio
-from abc import ABC
-
-class RepositorioProducto(Repositorio, ABC):
-    ...
-
-class RepositorioTipoProducto(Repositorio, ABC):
-    ...
